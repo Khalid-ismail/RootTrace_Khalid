43ca46f43770601647ec2d588d24f259893018b4
Khalid Ismail <khalid.n.ismail@gmail.com>
2016-02-09 13:40:07 +0000

    Adding RootTrace.h file

---
+++ Header_Files/RootTrace.h
@@ -0,0 +1,17 @@
+/*
+ * RootTrace.h
+ *
+ *  Created on: 9 Feb 2016
+ *      Author: Khalid Ismail
+ */
+
+#ifndef ROOTTRACE_H_
+#define ROOTTRACE_H_
+
+#include "MainCode.h"
+
+CMainCode m_main;
+
+
+
+#endif /* ROOTTRACE_H_ */
--- Source_Files/RootTrace.cpp
+++ Source_Files/RootTrace.cpp
@@ -2,11 +2,12 @@
 
 #include "stdafx.h"
 #include "Form1.h"
+#include "RootTrace.h"
 
 using namespace RootTrace2;
 
 // [STAThreadAttribute] //K_I
-int main(/*array<System::String ^> ^args*/) //K_I
+int main(int argc, char *argv[]/*array<System::String ^> ^args*/) //K_I
 {
 	
 	// Enabling Windows XP visual effects before any controls are created
@@ -14,9 +15,18 @@ int main(/*array<System::String ^> ^args*/) //K_I
 	//Application::SetCompatibleTextRenderingDefault(false); //K_I
 
 	// Create the main window and run it
-	Application::Run(gcnew Form1());
+	//Application::Run(gcnew Form1()); //K_I
+	int i;
 
-	mainprog->init(s, idealr, idealg, idealb, tb_lr, processX, processY/*, this->motionMixture*/, this);
+	char filename[100];
+	for(i=0;i<100;i++)
+	{
+		if(argv[1][i] == '.') {break;} // chop off file extension (if found)
+		else if(argv[1][i] == '\0'){break;}
+		else {filename[i] = argv[1][i];}
+	}
+
+	m_main.init(s, idealr, idealg, idealb, tb_lr, processX, processY/*, this->motionMixture*/, this);
 
 	return 0;
 }
